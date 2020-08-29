86860f69789848820debb82d6e4b386f9de3843d
SabrinaKaren <sabrinaa.karen@hotmail.com>
2020-08-28 23:23:10 -0300

    Recuperando restaurantes atraves de consulta HTTP/api rest

--- passaro-urbano/src/app/restaurant/restaurant.component.ts
+++ passaro-urbano/src/app/restaurant/restaurant.component.ts
@@ -1,15 +1,24 @@
+import { SalesService } from './../services/sales.service';
 import { Component, OnInit } from '@angular/core';
+import { Sale } from '../models/sale.model';
 
 @Component({
   selector: 'app-restaurant',
   templateUrl: './restaurant.component.html',
-  styleUrls: ['./restaurant.component.scss']
+  styleUrls: ['./restaurant.component.scss'],
+  providers: [ SalesService ]
 })
 export class RestaurantComponent implements OnInit {
 
-  constructor() { }
+  sales: Array<Sale>;
+
+  constructor(private salesService: SalesService) { }
 
   ngOnInit() {
+    this.salesService.getSalesByCategory('restaurante')
+        .then((sales: Sale[]) => {
+          this.sales = sales;
+        })
   }
 
 }
--- passaro-urbano/src/app/services/sales.service.ts
+++ passaro-urbano/src/app/services/sales.service.ts
@@ -10,7 +10,13 @@ export class SalesService {
   constructor(private http: HttpClient) { }
 
   getSales(): Promise<Array<Sale>> {
-    return this.http.get("http://localhost:3000/sales")
+    return this.http.get("http://localhost:3000/sales?featured=true")
+        .toPromise()
+        .then((response: any) => response);
+  }
+
+  getSalesByCategory(category: string){
+    return this.http.get(`http://localhost:3000/sales?category=${category}`)
         .toPromise()
         .then((response: any) => response);
   }
