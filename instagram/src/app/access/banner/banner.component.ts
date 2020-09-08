f96dbfc3e0cd9a0cdfc5a11b9c6a2e2603f54f93
SabrinaKaren <sabrinaa.karen@hotmail.com>
2020-09-08 19:41:20 -0300

    Rotatividade das imagens do banner de forma automática e "infinita"

--- instagram/src/app/access/banner/banner.component.ts
+++ instagram/src/app/access/banner/banner.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Image } from '../../models/image.model';
 
 @Component({
   selector: 'app-banner',
@@ -19,15 +20,41 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 })
 export class BannerComponent implements OnInit {
 
-  elementState = 'hidden';
+  state = 'visible';
+
+  images: Image[] = [
+    {state: 'visible', url: '/assets/banner-acesso/img_1.png'},
+    {state: 'hidden', url: '/assets/banner-acesso/img_2.png'},
+    {state: 'hidden', url: '/assets/banner-acesso/img_3.png'},
+    {state: 'hidden', url: '/assets/banner-acesso/img_4.png'},
+    {state: 'hidden', url: '/assets/banner-acesso/img_5.png'},
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => this.rotationLogic(), 2000);
   }
 
-  changeOfState(){
-    this.elementState = this.elementState === 'hidden' ? 'visible' : 'hidden';
+  rotationLogic(){
+
+    let nextImgIndex;
+    
+    // ocutando a imagem que está visível
+    for (let i=0; i<this.images.length; i++){
+      if (this.images[i].state === 'visible'){
+        this.images[i].state = 'hidden';
+        nextImgIndex = i === this.images.length-1 ? 0 : i+1;
+        break;
+      }
+    }
+
+    // tornando visível a próxima imagem do array
+    this.images[nextImgIndex].state = 'visible';
+
+    // tornando a função recursiva
+    setTimeout(() => this.rotationLogic(), 2000);
+
   }
 
 }
---
+++ instagram/src/app/models/image.model.ts
@@ -0,0 +1,6 @@
+export class Image {
+    constructor(
+        public state: string,
+        public url: string
+    ){}
+}
