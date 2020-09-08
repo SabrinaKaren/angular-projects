fcadf5541eabc62543f17ca73933f5cd68d0fc66
SabrinaKaren <sabrinaa.karen@hotmail.com>
2020-09-08 19:57:47 -0300

    Aplicando animação em um elemento de tipo componente

--- instagram/src/app/access/access.component.ts
+++ instagram/src/app/access/access.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-access',
   templateUrl: './access.component.html',
-  styleUrls: ['./access.component.scss']
+  styleUrls: ['./access.component.scss'],
+  animations: [
+    trigger('banner-animation', [
+      state('created', style({
+        opacity: 1
+      })),
+      transition('void => created', [
+        style({ opacity: 0, transform: 'translate(-50px, 0)' }),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AccessComponent implements OnInit {
+
+  bannerState = 'created';
 
   constructor() { }
 
